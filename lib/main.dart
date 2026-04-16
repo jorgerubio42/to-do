4bd4db38372732de09d4df94ab621f374cbd21e7
Jorge Rubio <246339851+jorgerubio42@users.noreply.github.com>
2026-04-16 10:36:55 -0600

    feat: add TextField for task input

--- lib/main.dart
+++ lib/main.dart
@@ -29,12 +29,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var _controller = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -44,22 +43,46 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: .center,
+              children: [
+                const Text('You wrote: '),
+                Text(
+                  _controller.text,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ]
+            )
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 0, 2.5, 5),
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter a task or idea',
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(2.5, 0, 5, 5),
+                child: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: (){
+                    setState((){});
+                  },
+                ),
+              ),
+            ]
+          )
+        ],
       ),
     );
   }
